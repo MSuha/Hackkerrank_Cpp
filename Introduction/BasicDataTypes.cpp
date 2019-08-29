6ed992e903c74700404c66308fd37af2e09ab8b5
muhammedsuhademirel <msuhademirel@gmail.com>
2019-08-29 14:56:49 +0300

    Basic Data Types

---
+++ Introduction/BasicDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int nmbr;
+    long lng;
+    char chr;
+    float flt;
+    double dbl;
+
+    //cin >> nmbr >> lng >> chr >> flt >> dbl;
+    //cout << nmbr << "\n" << lng << "\n" << chr << "\n" << flt << "\n" << dbl;
+
+    scanf("%d %ld %c %f %lf", &nmbr, &lng, &chr, &flt, &dbl);
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf", nmbr, lng, chr, flt, dbl);
+
+    return 0;
+}
+
