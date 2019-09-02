d17cdf6d2f4fbc01ba06e4ae8477c4a0d77f2607
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-02 15:38:57 +0300

    Vector Sort

--- STL/VectorSort.cpp
+++ STL/VectorSort.cpp
@@ -7,6 +7,27 @@ using namespace std;
 
 
 int main() {
+
+    int size, input;
+
+    cin>>size;
+
+    vector<int>my_vector(size);
+
+    for(int i = 0; i < size; i++){
+        cin>>input;
+        my_vector[i] = input;
+    }
+
+    sort(my_vector.begin(), my_vector.end());
+
+    for(int i = 0; i < size; i++){
+        cout<<my_vector[i]<<" ";
+    }
+
+
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
+
+
 }
