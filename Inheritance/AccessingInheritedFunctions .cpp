f0a6f76e0b6172366a63ffc44de072c60f5d0a11
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-12 16:33:17 +0300

    Accessing Inherited Functions

--- Inheritance/AccessingInheritedFunctions .cpp
+++ Inheritance/AccessingInheritedFunctions .cpp
@@ -1,114 +1,30 @@
-#include<iostream>
-
+#include <sstream>
+#include <vector>
+#include <iostream>
 using namespace std;
 
-class A
-{
-public:
-    A(){
-        callA = 0;
+vector<int> parseInts(string str) {
+    // Complete this function
+    vector<int> myVec;
+    stringstream ss(str);
+    char word;
+    int x;
+
+    while(ss >> x){
+        myVec.push_back(x);
+        ss >> word;
     }
-private:
-    int callA;
-    void inc(){
-        callA++;
+    return myVec;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
     }
 
-protected:
-    void func(int & a)
-    {
-        a = a * 2;
-        inc();
-    }
-public:
-    int getA(){
-        return callA;
-    }
-};
-
-class B
-{
-public:
-    B(){
-        callB = 0;
-    }
-private:
-    int callB;
-    void inc(){
-        callB++;
-    }
-protected:
-    void func(int & a)
-    {
-        a = a * 3;
-        inc();
-    }
-public:
-    int getB(){
-        return callB;
-    }
-};
-
-class C
-{
-public:
-    C(){
-        callC = 0;
-    }
-private:
-    int callC;
-    void inc(){
-        callC++;
-    }
-protected:
-    void func(int & a)
-    {
-        a = a * 5;
-        inc();
-    }
-public:
-    int getC(){
-        return callC;
-    }
-};
-
-class D : public A, public B, public C
-{
-
-    int val;
-public:
-    //Initially val is 1
-    D()
-    {
-        val = 1;
-    }
-
-
-    //Implement this function
-    void update_val(int new_val)
-    {
-        while(new_val > 1)
-        {
-            if(new_val%2 == 0)
-            {
-                A::func(val);
-                new_val /= 2;
-            }
-
-            if(new_val%3 == 0)
-            {
-                B::func(val);
-                new_val /= 3;
-            }
-
-            if(new_val%5 == 0)
-            {
-                C::func(val);
-                new_val /= 5;
-            }
-        }
-    }
-    //For Checking Purpose
-    void check(int); //Do not delete this line.
-};
+    return 0;
+}
 
